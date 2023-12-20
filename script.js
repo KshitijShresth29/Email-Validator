b0910439f7ce5cad61e75fa8b82b09940ecf93d9
Kshitij Shresth <145615126+KshitijShresth29@users.noreply.github.com>
2023-12-20 15:19:53 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("hi this is my script");
+let result =
+    {
+        "tag": "",
+        "free": true,
+        "role": false,
+        "user": "kshitijshresth2004",
+        "email": "kshitijshresth2004@gmail.com",
+        "score": 0.64,
+        "state": "deliverable",
+        "domain": "gmail.com",
+        "reason": "valid_mailbox",
+        "mx_found": true,
+        "catch_all": null,
+        "disposable": false,
+        "smtp_check": true,
+        "did_you_mean": "",
+        "format_valid": true
+      }
+let str = ``
+for(key of Object.keys(result)){
+    str=str+`<div>${key}:${result[key]}</div>`
+}
+console.log(str)
+resultcont.innerHTML=str
